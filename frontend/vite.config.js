9d14652b7bacdaef9bb893543e451b0dcb0c02c4
Karthik <karthik@indiatourpackages.com>
2026-03-01 19:20:44 +0530

    Change base path in Vite configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,7 @@ process.env.STORAGE_GATEWAY_URL =
   process.env.STORAGE_GATEWAY_URL || "https://blob.caffeine.ai";
 
 export default defineConfig({
-  base: './',
+  base: '/kerala/4n-5d/',
   logLevel: "error",
   build: {
     emptyOutDir: true,
