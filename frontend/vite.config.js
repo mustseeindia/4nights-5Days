d7a1dea86f937bdaa205dd13ebf7c98d14c9a558
Karthik <karthik@indiatourpackages.com>
2026-03-01 19:13:19 +0530

    Change base path in Vite config to relative

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,7 @@ process.env.STORAGE_GATEWAY_URL =
   process.env.STORAGE_GATEWAY_URL || "https://blob.caffeine.ai";
 
 export default defineConfig({
-  base: '/kerala/4n-5d/',
+  base: './',
   logLevel: "error",
   build: {
     emptyOutDir: true,
